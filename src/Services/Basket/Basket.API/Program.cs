b190dec41c9dfdbbc9c18962ac56e23ae82b600c
bagantonyan <bag.antonyan@gmail.com>
2024-06-19 14:01:10 +0300

    add basket microservive

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,33 +1,30 @@
+using Basket.API.Data;
+using Basket.API.Models;
 using BuildingBlocks.Behaviors;
-using Catalog.API.Data;
-using FluentValidation;
+using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container
-
-builder.Services.AddCarter();
-
 var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
-
-builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
-if (builder.Environment.IsDevelopment())
-    builder.Services.InitializeMartenWith<CatalogInitialData>();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline
 
 app.MapCarter();
 
